aa925341f0c0cdb559f87577e534961e104b5e47
Casey McCourt <cas.mcco@gmail.com>
2015-12-28 18:15:20 -0800

    view controller added to story board and view controller file added

---
+++ Terranaut/SnowViewController.swift
@@ -0,0 +1,28 @@
+//
+//  SnowViewController.swift
+//  Terranaut
+//
+//  Created by Casey McCourt on 12/28/15.
+//  Copyright © 2015 Apple Inc. All rights reserved.
+//
+
+import UIKit
+
+class SnowViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    // MARK: Properties
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+//    override func didReceiveMemoryWarning() {
+//        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+//    }
+    
+
+
+}
